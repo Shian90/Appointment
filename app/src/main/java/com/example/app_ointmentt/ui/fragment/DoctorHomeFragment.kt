0477c58f5b04726be6f794552d35c6d829d43c23
nafiz <naafizrahman1@gmail.com>
2020-05-17 18:33:20 +0600

    HomePage Doctor Modifications

--- app/src/main/java/com/example/app_ointmentt/ui/fragment/DoctorHomeFragment.kt
+++ app/src/main/java/com/example/app_ointmentt/ui/fragment/DoctorHomeFragment.kt
@@ -1,16 +1,18 @@
 package com.example.app_ointmentt.ui.fragment
 
-import android.content.Intent
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.app_ointmentt.Module
 import com.example.app_ointmentt.R
-import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_doctor_homepage.*
 import kotlinx.android.synthetic.main.fragment_doctor_homepage.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DoctorHomeFragment : Fragment() {
     override fun onCreateView(
@@ -18,22 +20,35 @@ class DoctorHomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view: View = inflater!!.inflate(R.layout.fragment_doctor_homepage, container, false)
-
-        view.logoutBtnDoctorHomePage.setOnClickListener { view ->
-            //logout()
+        val view: View = inflater.inflate(R.layout.fragment_doctor_homepage, container, false)
+        view.startTimeDefaultDoctorBtn.setOnClickListener {
+            showCalenderAndGetTime(it,startTimeDefaultDoctor)
         }
-        // Return the fragment view/layout
+        view.endTimeDefaultBtn.setOnClickListener{
+            showCalenderAndGetTime(it,endTimeDefault)
+        }
         return view
     }
-    private fun logout(){
 
-        FirebaseAuth.getInstance().signOut()
-        val intent = Intent(activity, Module::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(intent)
-        Toast.makeText(activity, "User has been logged out", Toast.LENGTH_SHORT).show()
-        activity?.finish()
+    private fun showCalenderAndGetTime(view: View?,textView: TextView) {
+        val cal = Calendar.getInstance()
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+            Toast.makeText(
+                view!!.context,
+                SimpleDateFormat("HH:mm", Locale.US).format(cal.time),
+                Toast.LENGTH_LONG
+            ).show()
+            textView.text = SimpleDateFormat("HH:mm", Locale.US).format(cal.time)
+        }
+        TimePickerDialog(
+            view!!.context,
+            timeSetListener,
+            cal.get(Calendar.HOUR_OF_DAY),
+            cal.get(Calendar.MINUTE),
+            false
+        ).show()
     }
 }
 
