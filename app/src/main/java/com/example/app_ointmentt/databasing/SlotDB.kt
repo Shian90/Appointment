17b6794dee7ddbb6fed4b6bba95f8e45bb8b1e00
hasnainkarim <47083396+hasnainkarim@users.noreply.github.com>
2020-09-13 22:06:06 +0600

    SlotDB/CreateSlot is implemented

--- app/src/main/java/com/example/app_ointmentt/databasing/SlotDB.kt
+++ app/src/main/java/com/example/app_ointmentt/databasing/SlotDB.kt
@@ -1,4 +1,96 @@
 package com.example.app_ointmentt.databasing
 
-class SlotDB {
+import android.content.Context
+import android.util.Log
+import android.preference.PreferenceManager
+import com.example.app_ointmentt.models.Doctor
+import com.example.app_ointmentt.models.Slot
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SlotDB(val context: Context) {
+
+    /***Create interfaces***/
+
+    //Create Slot interfaces
+    lateinit var mCreateSlotSuccessListener: createSlotSuccessListener
+    lateinit var mCreateSlotFailureListener: createSlotFailureListener
+
+
+    /***Calling API through functions***/
+    fun createSlot(date: String, startTime: String, endTime: String, numSlots: Int, status: Int)
+    {
+        val slotArray = arrayListOf<Slot>()
+
+        val sh = PreferenceManager.getDefaultSharedPreferences(context)
+        val jwt = sh.getString("jwt", "NONE FOUND").toString()
+        val uid = sh.getString("uid", "NONE FOUND").toString()
+        if ( jwt == "NONE FOUND" || uid == "NONE FOUND" )
+        {
+            //don't go any further
+            Log.d("CREATESLOTS", "$jwt $uid")
+            mCreateSlotFailureListener.createSlotFailure()
+        }
+        else
+        {
+            val paramsJSON = JSONObject()
+            paramsJSON.put("doctorId", uid)
+            paramsJSON.put("dateOfSlot", date)
+            paramsJSON.put("startTime", startTime)
+            paramsJSON.put("endTime", endTime)
+            paramsJSON.put("status", status.toString())
+            paramsJSON.put("numSlots", numSlots.toString())
+
+            val params = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"), paramsJSON.toString())
+
+            val headerJwt = "Bearer $jwt"
+
+            val call = APIObject.api.createSlot(headerJwt, params)
+
+            call.enqueue(object: Callback<ResponseBody> {
+                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                    val jsonRes = JSONObject(response.body()!!.string())
+                    val slots = jsonRes.getJSONArray("slots")
+                    Log.d("DEBUGSLOTS", "$slots")
+                    for ( i in 0 until slots.length() )
+                    {
+                        val slot = Slot().fromJSON(slots.getJSONObject(i))
+                        slotArray.add(slot)
+                    }
+                    mCreateSlotSuccessListener.createSlotSuccess(slotArray)
+                }
+
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    mCreateSlotFailureListener.createSlotFailure()
+                }
+            })
+        }
+    }
+
+    /***Interfaces***/
+    interface createSlotSuccessListener
+    {
+        fun createSlotSuccess(slotArray: ArrayList<Slot>)
+    }
+
+    interface createSlotFailureListener
+    {
+        fun createSlotFailure()
+    }
+
+    /***Interface setters***/
+    fun setCreateSlotSuccessListener(int: createSlotSuccessListener)
+    {
+        this.mCreateSlotSuccessListener = int
+    }
+
+    fun setCreateSlotFailureListener(int: createSlotFailureListener)
+    {
+        this.mCreateSlotFailureListener = int
+    }
+
 }
